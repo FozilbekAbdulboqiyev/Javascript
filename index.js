c69b5db8aefd08bda02872b9ee53e0bb4450e52f
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-04-09 16:04:25 +0500

    first java codes

---
+++ index.js
@@ -0,0 +1,125 @@
+// console.log( null || 2 && 3 || 4);
+// 3
+
+
+// console.log( console.log(1) && console.log(2) );
+// 3,1
+
+
+
+// console.log( 1 && null && 2 );
+// // null
+
+
+// console.log( console.log(1) || 2 || console.log(3) );
+// // 1 2
+
+
+// console.log( null || 2 || undefined );
+// 2
+
+
+// console.log(5 > 4)
+// true
+
+
+// console.log("apple" > "pineapple")
+// false
+
+
+// console.log(undefined == null)
+// true
+
+
+// console.log(undefined === null)
+// false
+
+// console.log(null == "\n0\n")
+// false
+
+
+// console.log(null === + "\n0\n")
+// false
+
+// console.log("" + 1 + 0)
+// 10
+
+
+// console.log("" - 1 + 0)
+// -1
+
+
+// console.log(true + false)
+// 1
+
+
+// console.log(6 / "3")
+// 2
+
+
+// console.log("2" * "3")
+// 6
+
+
+// console.log(4 + 5 + "px")
+// 9px
+
+
+// console.log("$" + 4 + 5)
+// $45
+
+
+// console.log("4" - 2)
+// 2
+
+
+// console.log("4px" - 2)
+// Nan
+
+
+// console.log("  -9  " +5)
+// -9 5
+
+
+// console.log("  -9  " -5)
+// -14
+
+
+// console.log(null + 1)
+// 1
+
+
+// console.log(undefined + 1)
+// NaN
+
+
+// console.log(" \t \n" - 2)
+// -2
+
+
+// let a1 = 2;
+// console.log(a1);
+// 2
+
+// let a=3
+// let x = 1 + (a *= 2)
+// console.log(x);
+// 7
+
+
+// let a = 1, b =1;
+// console.log(a);
+// console.log(b)
+// 11
+
+// let a = 2;
+// let c = ++a;
+// console.log(a)
+// console.log(c);
+// 3 3
+
+// let b = 3;
+// let d = b++;
+// console.log(b);
+// console.log(d);
+// 4 3
